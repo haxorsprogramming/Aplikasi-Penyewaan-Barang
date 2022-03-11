2f7d001cd29bed09d32729a5faad355918f1a1c6
Haxorsprogramming <haxors@uinsu.ac.id>
2022-03-11 08:47:28 +0700

    first commit

---
+++ AplikasiManajemenPenyewaanBarang/DatabaseClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace AplikasiManajemenPenyewaanBarang
+{
+    class DatabaseClass
+    {
+        public static MySqlConnection CONN_DB = new MySqlConnection("server=localhost;database=dbs_tahfiz_al_hafiz;uid=root;pwd=;");
+
+        public static void BUKA_KONEKSI()
+        {
+            Boolean status = true;
+            try
+            {
+                DatabaseClass.CONN_DB.Open();
+                status = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Koneksi gagal !!!");
+                status = false;
+            }
+            return;
+        }
+    }
+}
---
+++ AplikasiManajemenPenyewaanBarang/FrmLogin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AplikasiManajemenPenyewaanBarang
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+            DatabaseClass.BUKA_KONEKSI();
+        }
+    }
+}
